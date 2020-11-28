6544e8c552e456001a0775662120ff23cf76d90a
Limia188 <leolimia@gmail.com>
2020-11-28 18:59:26 -0300

    28/11/2020

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr jeringa' },
+  { id: 12, name: 'NarcoMexicano' },
+  { id: 13, name: 'Bombucha' },
+  { id: 14, name: 'Cerealitas' },
+  { id: 15, name: 'Magenga' },
+  { id: 16, name: 'Hombre tacho' },
+  { id: 17, name: 'Dynamita' },
+  { id: 18, name: 'Dr IVA' },
+  { id: 20, name: 'Fachachovich' },
+  { id: 19, name: 'Mathilda' },
+];
